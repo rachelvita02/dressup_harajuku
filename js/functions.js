5bd09360c094777f9f14dc7e54ba94f14743585d
Rachel Vita <vitar834@newschool.edu>
2024-10-15 10:34:24 -0400

    midterm update

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,15 @@ prefersDarkScheme.addListener((e) => {
     const newTheme = e.matches ? 'dark' : 'light';
     setTheme(newTheme);
 });
+
+/* sidebar menu */
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
