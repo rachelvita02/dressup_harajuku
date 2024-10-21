b8e78cc3d41c0e1c6b164f9c0e193a7f8f582194
Rachel Vita <vitar834@newschool.edu>
2024-10-21 14:02:27 -0400

    entry 3 & home page

--- js/functions.js
+++ js/functions.js
@@ -31,7 +31,8 @@ prefersDarkScheme.addListener((e) => {
     setTheme(newTheme);
 });
 
-/* sidebar menu */
+/* sidebar menu 
+________________________________*/
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
@@ -42,3 +43,29 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+/* image gallery
+________________________________*/
+let slideIndex = 1;
+        showSlides(slideIndex);
+        function plusSlides(n) {
+          showSlides(slideIndex += n);
+        }
+        function currentSlide(n) {
+          showSlides(slideIndex = n);
+        }
+        function showSlides(n) {
+          let i;
+          let slides = document.getElementsByClassName("mySlides");
+          let dots = document.getElementsByClassName("dot");
+          if (n > slides.length) {slideIndex = 1}    
+          if (n < 1) {slideIndex = slides.length}
+          for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+          }
+          for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+          }
+          slides[slideIndex-1].style.display = "block";  
+          dots[slideIndex-1].className += " active";
+        }
